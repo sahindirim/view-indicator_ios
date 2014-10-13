831c432740ec871e690ca9ed11d118472bdeae27
Şahin Dirim <sahindirim@Sahin-MacBook-Pro.local>
2014-10-13 11:49:44 +0300

    Initial Commit

---
+++ view indicator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  view indicator
+//
+//  Created by Şahin Dirim on 13.10.2014.
+//  Copyright (c) 2014 Şahin Dirim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
